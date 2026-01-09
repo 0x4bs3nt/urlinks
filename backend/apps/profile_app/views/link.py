4283a0b1d7a8433ab4322cfccd7b50613068f099
0x4bs3nt <hi@4bs3nt.com>
2026-01-09 18:30:32 +0100

    feat(backend): validation on links

--- backend/apps/profile_app/views/link.py
+++ backend/apps/profile_app/views/link.py
@@ -1,6 +1,7 @@
 from apps.profile_app.models.link import Link
 from apps.profile_app.serializers.link import LinkSerializer
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 
@@ -17,3 +18,26 @@ class LinkViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Link.objects.filter(page__user=self.request.user)
+
+    def perform_create(self, serializer):
+        page = serializer.validated_data.get("page")
+        if page.user != self.request.user:
+            raise PermissionDenied("You can only add links to your own page.")
+
+        serializer.save()
+
+    def perform_update(self, serializer):
+        if serializer.instance.page.user != self.request.user:
+            raise PermissionDenied("You can only edit your own links.")
+        new_page = serializer.validated_data.get("page")
+
+        if new_page and new_page.user != self.request.user:
+            raise ValidationError({"page": "You can only move links to your own page."})
+
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.page.user != self.request.user:
+            raise PermissionDenied("You can only delete your own links.")
+
+        instance.delete()
