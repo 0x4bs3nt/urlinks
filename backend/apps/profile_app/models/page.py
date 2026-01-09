92649a0de0fd107489a971a66a682bfb4b2d442a
0x4bs3nt <hi@4bs3nt.com>
2026-01-09 19:13:02 +0100

    feat(storages): upload backend path for uploading files

--- backend/apps/profile_app/models/page.py
+++ backend/apps/profile_app/models/page.py
@@ -2,6 +2,14 @@ from apps.core.models import CustomUser
 from django.db import models
 
 
+def profile_picture_path(instance, filename):
+    return f"folder_{instance.user_id}/profile_pictures/{filename}"
+
+
+def banner_picture_path(instance, filename):
+    return f"folder_{instance.user_id}/banners/{filename}"
+
+
 class Page(models.Model):
     user = models.OneToOneField(
         CustomUser, on_delete=models.CASCADE, related_name="page"
@@ -9,10 +17,10 @@ class Page(models.Model):
     display_name = models.SlugField(max_length=30)
     bio = models.CharField(max_length=200, blank=True, null=True)
     profile_picture = models.ImageField(
-        upload_to="pages/profile_pictures/", blank=True, null=True
+        upload_to=profile_picture_path, blank=True, null=True
     )
     banner_picture = models.ImageField(
-        upload_to="pages/banners/", blank=True, null=True
+        upload_to=banner_picture_path, blank=True, null=True
     )
     is_published = models.BooleanField(default=False)
 
@@ -27,4 +35,4 @@ class Page(models.Model):
         if self.banner_picture:
             self.banner_picture.delete(save=False)
 
-        super().delete(*args, **kwargs)
+        return super().delete(*args, **kwargs)
